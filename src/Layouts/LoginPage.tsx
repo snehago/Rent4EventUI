d64389e49d641c03e8229b598d6bfaaa8702687d
Jaiswal <vinajaiswal@deloitte.com>
2021-08-20 14:31:25 +0530

    Designed Login And Register Page

--- src/Layouts/LoginPage.tsx
+++ src/Layouts/LoginPage.tsx
@@ -1,6 +1,104 @@
 import React from "react";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import LockOpenOutlinedIcon from "@material-ui/icons/LockOpenOutlined";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "20px auto",
+  };
+  const avatarStyle = {
+    backgroundColor: "#1bbd7e",
+  };
+  const btnStyle = { margin: "8px 0" };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailHandler = (event: any) => {
+    setEmail(event.target.value)
+  };
+  const passwordHandler = (event: any) => {
+    setPassword(event.target.value)
+  };
+  const handleSubmit=()=>{
+    console.log("Email:"+email)
+    console.log("Password:"+password)
+  }
+  return (
+    <Grid>
+      <Paper elevation={10} style={paperStyle}>
+        <Grid style={{ display:'flex',flexDirection:"column",alignItems:'center' }} >
+          <Avatar style={avatarStyle}>
+            <LockOpenOutlinedIcon />
+          </Avatar>
+          <h2>Sign In</h2>
+        </Grid>
+        <TextField
+          value={email}
+          onChange={emailHandler}
+          label="Email"
+          placeholder="Enter Email"
+          variant="outlined"
+          fullWidth
+          required
+          style={{ marginBottom: "5%" }}
+        />
+        <TextField
+          value={password}
+          onChange={passwordHandler}
+          label="Password"
+          placeholder="Enter Password"
+          type="password"
+          variant="outlined"
+          fullWidth
+          required
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="checkedb"
+              color="primary"
+            />
+          }
+          label="Remember Me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          color="primary"
+          style={btnStyle}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Sign In
+        </Button>
+        <Typography>
+          <Link href="#">Forgot Password ?</Link>
+        </Typography>
+        <Typography>
+          {" "}
+          Do you have an account ?
+          <NavLink style={{textDecoration:"none"}}  exact to="/user/register/">
+            Register
+          </NavLink>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
 };
 export default LoginPage;
--- src/Layouts/RegistrationPage.tsx
+++ src/Layouts/RegistrationPage.tsx
@@ -1,6 +1,154 @@
-import React from "react";
-
+import { Avatar, Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 const RegistrationPage = () => {
-  return <div>Registration</div>;
+  const paperStyle = {
+    padding: "30px 20px",
+    width: 500,
+    margin: "20px auto",
+  };
+  const headerStyle = {
+    margin: 0,
+  };
+  const avatarStyle = {
+    backgroundColor: "#1bbd7e",
+  };
+  const textFieldStyle = {
+    marginBottom: "5%",
+  };
+  const btnStyle = { margin: "8px 0" };
+
+
+  const [role,setRole]=useState('host')
+  return (
+    <Grid>
+      <Paper elevation={20} style={paperStyle}>
+        <Grid
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar style={avatarStyle}>
+            <AddCircleOutlineOutlinedIcon />
+          </Avatar>
+          <h2 style={headerStyle}>Register</h2>
+          <Typography variant="caption">
+            Please fill this form to create an account !
+          </Typography>
+        </Grid>
+
+        <form>
+          <TextField
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            label="First Name"
+            placeholder="Enter your First name"
+            required
+          />
+          <TextField
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            label="Last Name"
+            placeholder="Enter your Last name"
+            required
+          />
+          <TextField
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            label="Email"
+            placeholder="Enter your Email Id"
+            required
+          />
+          <TextField
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            type="password"
+            label="Password"
+            placeholder="Enter a Password"
+            required
+          />
+          <TextField
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            type="password"
+            label="Confirm Password"
+            placeholder="Confirm your Password"
+            required
+          />
+          <TextField
+            variant="outlined"
+            fullWidth
+            style={textFieldStyle}
+            id="date"
+            label="Date Of Birth"
+            placeholder="Select your Date Of Birth"
+            type="date"
+            defaultValue="2021-08-10"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            required
+          />
+
+          <FormControl component="fieldset" required style={textFieldStyle}>
+            <FormLabel component="legend">Role</FormLabel>
+            <RadioGroup
+              aria-label="role"
+              name="role1"
+              style={{display:"initial"}}
+              // value={value}
+              // onChange={handleChange}
+            >
+              <FormControlLabel
+                value="user"
+                control={<Radio />}
+                label="User"
+              />
+              <FormControlLabel value="host" control={<Radio />} label="Host" />
+         
+            </RadioGroup>
+          </FormControl>
+
+          {
+            role==='host'
+            ?
+            <TextField
+            required
+            style={textFieldStyle}
+            variant="outlined"
+            fullWidth
+            label="Upi Id"
+            placeholder="Give your Upi Id"
+            />
+            :
+            null
+          }
+        </form>
+
+        <Button
+          type="submit"
+          fullWidth
+          color="primary"
+          style={btnStyle}
+          variant="contained"
+          // onClick={handleSubmit}
+        >
+          REGISTER
+        </Button>
+      </Paper>
+    </Grid>
+  );
 };
 export default RegistrationPage;
