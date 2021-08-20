8d2bb212caee30d0774c1ae5bf01a8e4eb5f00f3
Neema, Anuj <anuneema@deloitte.com>
2021-08-20 12:01:02 +0530

    .env fixed along with service example

--- src/Layouts/HomePage.tsx
+++ src/Layouts/HomePage.tsx
@@ -1,6 +1,22 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { UserService } from "../Services/UserService";
+const userService = new UserService();
 const HomePage = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const response = await userService.login({
+        id: 0,
+        firstName:"",
+        lastName:"",
+        dateOfBirth: new Date(),
+        email:"abc@gmail.com",
+        passwordHash: "12345678",
+        role: "host"
+      });
+      console.log(response);
+    })();
+  });
   return <div>Home Page</div>;
 };
 export default HomePage;
--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -3,11 +3,11 @@ import { User } from "../Shared/Interfaces/User";
 import { SharedService } from "./SharedService";
 const sharedService = new SharedService();
 class UserService {
-  BACKEND_URL: string | undefined = process.env.BACKEND_URL;
+  BACKEND_URL: string | undefined = process.env.REACT_APP_BACKEND_URL;
 
   public async signup(user: User) {
     return axios.post(
-      `${this.BACKEND_URL}`,
+      `${this.BACKEND_URL}/user`,
       user,
       await sharedService.getHeader()
     );
@@ -15,7 +15,7 @@ class UserService {
 
   public async login(user: User) {
     return axios.post(
-      `${this.BACKEND_URL}/login`,
+      `${this.BACKEND_URL}/user/login`,
       user,
       await sharedService.getHeader()
     );
