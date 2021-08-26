d8a12f610dde50c21b287c35d99546310f04a22f
Neema, Anuj <anuneema@deloitte.com>
2021-08-26 15:38:17 +0530

    checkout page created

--- src/Layouts/CheckoutPage.tsx
+++ src/Layouts/CheckoutPage.tsx
@@ -5,11 +5,16 @@ import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import { of } from 'await-of';
 import { Venue } from '../Shared/Interfaces/Venue';
+import SimpleModal from '../Components/Modal'
+import { useHistory } from 'react-router';
+import './styles/checkoutPage.scss';
+import { Button, Card, Divider, Grid, TextField } from '@material-ui/core';
 const venueService = new VenueService();
 export default function CheckoutPage() {
   const { venueId } = useParams<any>();
   const [venue, setVenue]= useState<Venue|null>(null);
-  
+  const [open, setOpen]= useState<boolean>(false);
+  const history = useHistory();
   useEffect(()=>{
     (async ()=> {
       const [response,error] = await of(venueService.getVenueByVenueId(venueId));
@@ -21,19 +26,115 @@ export default function CheckoutPage() {
       }
     })();
   },[venueId]);
-  
+  const onModalClose = ()=> {
+    setOpen(false);
+    history.push("/home");
+  }
+  const onSubmit = (data:any) => {
+    console.log(data);
+    data.preventDefault();
+    setOpen(true);
+  }
   return (
     <>
       <header>
         <Header></Header>
       </header>
-      <div className="main-content">
-        {JSON.stringify(venue)}
+      <div className="checkout-main-content">
+        <Grid container spacing={2} className="venue-details-card">
+          <Grid item lg={6}>
+            <Card>
+              <Grid container spacing={4}>
+                <Grid item lg={6}>
+                  Venue Name
+                </Grid>
+                <Grid item lg={6}>
+                  {venue?.title.toUpperCase()}
+                </Grid>
+                <Grid item lg={6}>
+                  Description
+                </Grid>
+                <Grid item lg={6}>
+                  {venue?.description}
+                </Grid>
+                <Grid item lg={6}>
+                  Capacity
+                </Grid>
+                <Grid item lg={6}>
+                  {venue?.capactiy}
+                </Grid>
+                <Grid item lg={6}>
+                  Contact Details
+                </Grid>
+                <Grid item lg={6}>
+                  {venue && `${venue?.host.firstName} ${venue?.host.lastName}`}
+                </Grid>
+                <Grid item lg={12}>
+                  <Divider/>
+                </Grid>
+                <Grid item lg={10}>
+                  Total Price:
+                </Grid>
+                <Grid item lg={2}>
+                  ${venue?.price}
+                </Grid>
+                <Grid item lg={10}>
+                  Booking amount:
+                </Grid>
+                <Grid item lg={2}>
+                  ${venue && (venue?.price * 10) / 100}
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item lg={5} className="form-container">
+            <form onSubmit={(data:any) => onSubmit(data)}>
+              <Grid container direction="column" spacing={4}>
+                <Grid item lg={12}>
+                  <TextField
+                    id="contactNumber"
+                    name="contactNumnber"
+                    label="Contact Number"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item lg={12}>
+                  <TextField
+                    id="numberOfAttendees"
+                    name="numberOfAttendees"
+                    label="Number of Attendees"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                  >
+                    Proceed To Pay
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
+        <div>
+          <SimpleModal 
+            title="Booking successfull"
+            content={`Your booking for venue is completed your booking id is ${venueId}`}
+            open={open}
+            closeCallback={onModalClose}
+          />
+        </div>
       </div>
+
       <footer>
         <Footer></Footer>
       </footer>
-      
     </>
   );
 }
